8ee8318d10969d614ed2caa5e76d787f95c0a122
Taras Shpot <mrshpot@gmail.com>
2012-08-14 18:38:45 +0300

    Add LICENSE and license headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2012, Taras Shpot
+ * All rights reserved. Email: mrshpot@gmail.com
+ * 
+ * This demo is free software; you can redistribute it and/or modify
+ * it under the terms of the BSD-style license that is included in the
+ * file LICENSE.
+ */
+
 #include <cstdio>
 #include <ctime>
 #include <cmath>
