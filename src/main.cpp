372f98abda341a4b51d64b59e0b50f4822631c74
Taras Shpot <mrshpot@gmail.com>
2012-08-27 07:31:10 +0300

    main: Don't crash when no script is loaded

--- src/main.cpp
+++ src/main.cpp
@@ -201,7 +201,10 @@ void update()
 
     if (g_update)
     {
-        g_script->update(0.01f);
+        if (g_script != NULL)
+        {
+            g_script->update(0.01f);
+        }
         g_cloth->step(0.01f);
     }
     
